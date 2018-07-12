442aaad96d6bbe365bbb644f87ef9602fd3098ef
Surya Murali <msurya25@gmail.com>
2018-07-12 16:17:12 -0700

    Update TE2.py

--- TE2.py
+++ TE2.py
@@ -15,12 +15,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from collections import Counter
+import matplotlib.pyplot as plt
+# import seaborn as sns
+import pickle
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+import gensim
+from gensim import corpora, models
+from gensim.models.coherencemodel import CoherenceModel
+from numpy import array
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 
 domains_list = []
 messages = []
 
 def clean_str(string):
     string = string.replace("_", " ")
+    string = string.replace("'", " ")
+    string = string.replace(",", " ")
     string = re.sub(re.compile("<math>.*?</math>"), " ", string)
     string = re.sub(re.compile("<url>.*?</url>"), " ", string)
     string = re.sub(re.compile("<.*?>"), " ", string)
@@ -41,76 +55,94 @@ def clean_str(string):
     string = re.sub(r"\?", " \? ", string)
     return string
 
-def pre_process_texts(text_file):
-    text = ""
-    with open("/Users/PycharmProjects/TextFiles/" + text_file + ".txt") as fp:
-        for line in islice(fp, 2, None):
-            text = text + line
+def preprocess_data(csv_file):
+    # Read the list of domains you want to scrape. The CSV contains the the domains list with the header 'names'
+    # domains_data_frame = pd.read_csv("/Users/sm912r/PycharmProjects/Model_LDA.csv")
+    doc_list = []
+    text_list = []
+    domains_list = []
+    category_list = []
+    domains_data_frame = pd.read_csv("/Users/sm912r/PycharmProjects/" + csv_file + ".csv")
 
-    text = clean_str(text)
-    text = text.lower()
+    # For loop iterating the list of URLs
+    for i in range(0, len(domains_data_frame)):
+        initial_domain = (domains_data_frame.loc[i, 'names'])
+        # print(initial_domain)
+        initial_category = (domains_data_frame.loc[i, 'category'])
+        text = ""
+        try:
+            with open("/Users/sm912r/PycharmProjects/TextFiles_Jun28/"+initial_domain+".txt") as fp:
+                for line in islice(fp, 2, None):
+                    text = text + line
+        except:
+            # print("Exception domain: ", initial_domain)
+            domains_data_frame.drop(domains_data_frame.index[i])
+            continue
+        # print(len(text))
+        if(len(text)<900):
+            # print(len(text))
+            # print(initial_domain)
+            domains_data_frame.drop(domains_data_frame.index[i])
+            continue
+        domains_list.append(initial_domain)
+        category_list.append(initial_category)
+        text = clean_str(text)
+        text = text.lower()
+        words = word_tokenize(text)
+        stop_words = set(stopwords.words("english"))
+        filtered_sentence = []
+        for w in words:
+            if w not in stop_words:
+                filtered_sentence.append(w)
+        ps = PorterStemmer()
+        stemmed = []
+        for w in filtered_sentence:
+            stemmed.append(w)
+        allowed = set(ascii_letters)
+        output = [word for word in stemmed if any(letter in allowed for letter in word)]
+        new = ""
+        for i in range(len(output)):
+            new = new + " " + output[i]
+        new = new.encode("ascii", errors="ignore").decode()
+        result = ''.join([i for i in new if not i.isdigit()])
+        # Remove words less than 3 characters long
+        result = re.sub(r'\b\w{1,2}\b', '', result)
+        result = ' '.join(word for word in result.split() if len(word) < 15)
+        text_list.append(result)
+        # Tokenizinggggggggg
+        words_tok = word_tokenize(result)
+        doc_list.append(words_tok)
+    return text_list, doc_list, domains_list, category_list
 
-    words = word_tokenize(text)
+train_text_list, train_doc_list, train_domains_list, train_category_list = preprocess_data("Train")
 
-    stop_words = set(stopwords.words("english"))
+print("\nNumber of Training data: ", len(train_domains_list), "\n")
+Personal_Finance_Train_Records = sum(train_category_list)
+print("Number of Personal Finance Training Records: ", Personal_Finance_Train_Records)
+print("Number of Non-Personal Finance Training Records: ", len(train_domains_list)-Personal_Finance_Train_Records)
 
-    filtered_sentence = []
+original_text = ""
+for i in range(0, len(train_text_list)):
+    original_text = original_text + " " + train_text_list[i]
 
-    for w in words:
-        if w not in stop_words:
-            filtered_sentence.append(w)
+rare_words_list = []
+counts = Counter(word_tokenize(original_text))
+for i in counts:
+    if counts[i]<1:
+        rare_words_list.append(i)
 
-    # print(filtered_sentence)
-
-    ps = PorterStemmer()
-
-    stemmed = []
-
-    for w in filtered_sentence:
-        stemmed.append(w)
-
-    allowed = set(ascii_letters)
-    output = [word for word in stemmed if any(letter in allowed for letter in word)]
-
-    new = ""
-    for i in range(len(output)):
-        new = new + " " + output[i]
-    new = new.encode("ascii", errors="ignore").decode()
-    result = ''.join([i for i in new if not i.isdigit()])
-    # Remove words less than 3 characters long
-    result = re.sub(r'\b\w{1,2}\b', '', result)
-    result = ' '.join(word for word in result.split() if len(word) < 15)
-    return result
-
-# Read the list of domains you want to scrape. The CSV contains the the domains list with the header 'names'
-domains_data_frame = pd.read_csv("/Users/sm912r/PycharmProjects/Model.csv")
-
-# Print the domains
-print("\nList of websites to be classified: \n", domains_data_frame)
-
-# Print the number of domains to be scrapped
-print("\nNumber of websites to be classified: ", len(domains_data_frame), "\n")
-
-# For loop iterating the list of URLs
-for i in range(0, len(domains_data_frame)):
-    initial_domain = (domains_data_frame.loc[i, 'names'])
-    # print(initial_domain)
-    initial_category = (domains_data_frame.loc[i, 'category'])
-    # print(initial_category)
-    messages.append(pre_process_texts(initial_domain))
-
-# print(messages)
+print("Number of rare words: \n", len(rare_words_list))
 
 vect = CountVectorizer()
 #Using the fit method, our CountVectorizer() will “learn” what tokens are being used in our messages.
-vect.fit(messages)
+# vect.fit(messages)
+vect.fit(train_text_list)
 
 # to see what tokens have been “learned” by CountVectorizer
 # print(vect.get_feature_names())
 
-doc_term_matrix = vect.transform(messages)
+doc_term_matrix = vect.transform(train_text_list)
 repr(doc_term_matrix)
-
 # print(doc_term_matrix) #sparse matrix
 
 #document term matrix
@@ -118,7 +150,7 @@ repr(doc_term_matrix)
 #rows correspond to documents in the collection and columns correspond to terms
 
 
-#n order to save space/computational power a sparse matrix is created.
+#In order to save space/computational power a sparse matrix is crea sted.
 #This means that only the location and value of non-zero values is saved.
 #it’s advisable to keep it in sparse form especially when working with a large corpus.
 
@@ -135,190 +167,3 @@ df = pd.DataFrame(doc_term_matrix.toarray(), columns=vect.get_feature_names())
 
 #If we have several occurences of the same word in one document we can expect the TF-IDF to *increase*.
 #If a word is used in many documents then the TF-IDF will *decrease*.
-
-def createDTM(messages):
-    vect = TfidfVectorizer()
-    doc_term_matrix = vect.fit_transform(messages)  # create DTM
-
-    # create pandas dataframe of DTM
-    return pd.DataFrame(doc_term_matrix.toarray(), columns=vect.get_feature_names())
-
-# 6th document list of words
-# print(createDTM(messages).iloc[5])
-
-data = createDTM(messages)
-data = pd.concat((data, domains_data_frame['category']), axis = 1)
-# print(data.sum(axis=0))
-# print(data.head())
-# print(len(data.columns))
-
-train_features = data.iloc[:,0:len(data.columns)-1]
-train_target = data.iloc[:, len(data.columns)-1:]
-# print(train_features)
-# print(train_target)
-
-logreg = LogisticRegression()
-logreg.fit(train_features, train_target.values.ravel())
-
-print("TESTING SET")
-
-test_domains_data_frame = pd.read_csv("/Users/sm912r/PycharmProjects/Model_test.csv")
-print("\nList of websites to be classified: \n", test_domains_data_frame)
-
-# Print the number of domains to be scrapped
-print("\nNumber of websites to be classified: ", len(test_domains_data_frame), "\n")
-
-messages_test = []
-
-for i in range(0, len(test_domains_data_frame)):
-    initial_domain_test = (test_domains_data_frame.loc[i, 'names'])
-    # print(initial_domain_test)
-    initial_category_test = (test_domains_data_frame.loc[i, 'category'])
-    # print(initial_category)
-    messages_test.append(pre_process_texts(initial_domain_test))
-
-vect_test = CountVectorizer()
-vect_test.fit(messages_test)
-
-doc_term_matrix_test = vect_test.transform(messages_test)
-repr(doc_term_matrix_test)
-
-df_test = pd.DataFrame(doc_term_matrix_test.toarray(), columns=vect_test.get_feature_names())
-
-def createDTM_test(messages_test):
-    vect_test = TfidfVectorizer()
-    doc_term_matrix = vect_test.fit_transform(messages_test)  # create DTM
-
-    # create pandas dataframe of DTM
-    return pd.DataFrame(doc_term_matrix_test.toarray(), columns=vect_test.get_feature_names())
-
-test_data = createDTM_test(messages_test)
-test_data = pd.concat((test_data, test_domains_data_frame['category']), axis = 1)
-
-print("Training: \n")
-print(data.head())
-print(len(data.columns))
-print(list(data))
-
-print("Testing: \n")
-print(test_data.head())
-print(len(test_data.columns))
-print(list(test_data))
-
-t3 = data[:len(test_data)]
-
-t3 = pd.DataFrame(np.zeros((t3.shape[0], t3.shape[1])))
-t3.columns = data.columns
-print(t3)
-
-new_list = []
-
-new_list = [item for item in test_data if item in data]
-
-for i in new_list:
-    t3[i] = test_data[i]
-
-print(len(t3))
-
-
-
-
-
-
-test_features = t3.iloc[:,0:len(t3.columns)-1]
-test_target = t3.iloc[:, len(t3.columns)-1:]
-# print(test_features)
-# print(test_target)
-
-test_pred = logreg.predict(test_features)
-
-from sklearn.metrics import accuracy_score
-# accuracy_score(test_target, y_pred)
-
-print("Accuracy: ", str(round(100*accuracy_score(test_target, test_pred), 2)) + " %")
-
-from sklearn.metrics import confusion_matrix
-confusion_matrix = confusion_matrix(test_target, test_pred)
-print("Confusion Matrix: \n", confusion_matrix)
-
-from sklearn.metrics import classification_report
-print("Classification Report: \n", classification_report(test_target, test_pred))
-
-import matplotlib.pyplot as plt
-plt.rc("font", size=14)
-
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import roc_curve
-logit_roc_auc = roc_auc_score(test_target, logreg.predict(test_features))
-fpr, tpr, thresholds = roc_curve(test_target, logreg.predict_proba(test_features)[:,1])
-plt.figure()
-plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.savefig('Log_ROC')
-plt.show()
-
-# from sklearn import model_selection
-# from sklearn.model_selection import cross_val_score
-# kfold = model_selection.KFold(n_splits=10, random_state=None, shuffle = True)
-# # modelCV = LogisticRegression()
-# scoring = 'accuracy'
-# results = model_selection.cross_val_score(logreg, data.iloc[:,0:len(train.columns)-1], data.iloc[:,len(train.columns)-1].values.ravel(), cv=kfold, scoring=scoring)
-# # print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
-# print("10-fold cross validation average accuracy: ", str(round(100*results.mean(), 2)) + " %" )
-
-"""
-test_features = test.iloc[:,0:len(test.columns)-1]
-test_target = test.iloc[:, len(test.columns)-1:]
-# print(test_features)
-# print(test_target)
-
-logreg = LogisticRegression()
-logreg.fit(train_features, train_target.values.ravel())
-
-test_pred = logreg.predict(test_features)
-
-from sklearn.metrics import accuracy_score
-# accuracy_score(test_target, y_pred)
-
-print("Accuracy: ", str(round(100*accuracy_score(test_target, test_pred), 2)) + " %")
-
-from sklearn.metrics import confusion_matrix
-confusion_matrix = confusion_matrix(test_target, test_pred)
-print("Confusion Matrix: \n", confusion_matrix)
-
-from sklearn.metrics import classification_report
-print("Classification Report: \n", classification_report(test_target, test_pred))
-
-import matplotlib.pyplot as plt
-plt.rc("font", size=14)
-
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import roc_curve
-logit_roc_auc = roc_auc_score(test_target, logreg.predict(test_features))
-fpr, tpr, thresholds = roc_curve(test_target, logreg.predict_proba(test_features)[:,1])
-plt.figure()
-plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.savefig('Log_ROC')
-plt.show()
-
-from sklearn import model_selection
-from sklearn.model_selection import cross_val_score
-kfold = model_selection.KFold(n_splits=10, random_state=None, shuffle = True)
-# modelCV = LogisticRegression()
-scoring = 'accuracy'
-results = model_selection.cross_val_score(logreg, data.iloc[:,0:len(train.columns)-1], data.iloc[:,len(train.columns)-1].values.ravel(), cv=kfold, scoring=scoring)
-# print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
-print("10-fold cross validation average accuracy: ", str(round(100*results.mean(), 2)) + " %" ) """""
