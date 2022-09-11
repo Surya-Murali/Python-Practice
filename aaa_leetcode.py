bae95933c38580f34cc8e3ff0a058a019af3d634
Surya Murali <msurya25@gmail.com>
2022-09-10 22:45:23 -0700

    Update aaa_leetcode.py

--- aaa_leetcode.py
+++ aaa_leetcode.py
@@ -1,1 +1,31 @@
+# TWO SUMS:
+https://leetcode.com/problems/two-sum/
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        d={}
+        for i,num in list(enumerate(nums)):
+            print(list(enumerate(nums)))
+            print(target-num)
+            if target-num in d:
+                print(i)
+                print(d)
+                print(d[target-num])
+                return d[target-num], i
+            d[num]=i
+
+# # INPUT:
+# [2,7,11,15]
+# 9
+
+# # OUTPUT:
+# [(0, 2), (1, 7), (2, 11), (3, 15)]
+# 7
+# [(0, 2), (1, 7), (2, 11), (3, 15)]
+# 2
+# 1
+# {2: 0}
+# 0
+
+# # FINAL OUTPUT:
+# (0,1)
