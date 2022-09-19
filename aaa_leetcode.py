9d2037f039dc6d743629f3c201e20548c522e877
Surya Murali <msurya25@gmail.com>
2022-09-18 22:53:35 -0700

    Update aaa_leetcode.py

--- aaa_leetcode.py
+++ aaa_leetcode.py
@@ -102,3 +102,87 @@ class Solution(object):
             return False
         else:
             return True
+
+# https://leetcode.com/problems/palindrome-number/submissions/
+# Reverse a string or Integer:
+# Hint: 
+# Use pop() - removes last element from the list
+# For loop should have range then
+# ''.join(l2) -> convert list to String!
+
+class Solution(object):
+    def isPalindrome(self, x):
+        l1 = list(str(x))
+        l2 = []
+        for i in range(0, len(l1)):
+            l2.append(l1.pop())
+        if ''.join(l2) == str(x):
+            return True
+        else:
+            return False
+        
+# Reversing without String conversion:
+# Get the last digit
+# Then multiply by 10 , add the last digit and so on
+# Divide the number by 10 at the end before getting the last digit
+# While loop needed - while number not equal to 0
+
+class Solution(object):
+    def isPalindrome(self, x):
+        reversed_num = 0
+        while x != 0:
+            print("X Value: ", x)
+            digit = x % 10
+            reversed_num = reversed_num * 10 + digit
+            print("Rev: ", reversed_num)
+            x = x/10
+        print(reversed_num)
+
+# Finding out that its not a palindrome early!
+class Solution(object):
+    def isPalindrome(self, x):
+         if(x>=0):
+            y = str(x)
+            for i in range(0, len(y)/2):
+                if y[i] != y[len(y)-1-i]:
+                    return False
+            return True
+        
+# Remove duplicates from a list
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# Used this code because its non-decreasing
+
+# List Functions
+# Remove Duplicate
+
+#(1) Using 'set' - But order is not maintained
+
+l = [1,9,2,1,2]
+list(set(l))
+# [1, 2, 9]
+list(set('azyxbc'))
+# ['a', 'c', 'b', 'y', 'x', 'z']
+
+#(2) Using 'dict.fromkeys' - Again order is not maintained
+list(dict.fromkeys(l))
+# [1, 2, 9]
+
+#(3) Using 'OrderedDict' - Order is maintained
+from collections import OrderedDict
+list(OrderedDict.fromkeys(l))
+# [1, 9, 2]
+
+# OrderedDict and dict.fromkeys also work on strings
+OrderedDict.fromkeys('azyxbc')
+# ['a', 'z', 'y', 'x', 'b', 'c']
+
+# Sorting of Lists
+l = [1, 9, 2, 1, 2]
+
+# Ascending Sort:
+l.sort()
+
+# Descending Sort:
+l.sort(reverse=True)
+l.reverse()
+
